bda81c66e3c663f9764dbe5c76a6d46c22117db1
Shawn Lin <xiaoliang.lin@columbia.edu>
2019-02-23 23:06:36 -0500

    inital commit

---
+++ eod_price.sql
@@ -0,0 +1,10 @@
+USE `DW`;
+CREATE  OR REPLACE VIEW `eod_price` AS
+(SELECT main.tradeday, main.ticker, open, high, low, close, adj, volume, dividend
+  FROM DW.eod_price_archive AS main
+ INNER JOIN 
+	(SELECT tradeday, ticker, MAX(modified) AS modified
+	   FROM DW.eod_price_archive
+	  GROUP BY tradeday, ticker) AS aux
+	ON main.tradeday = aux.tradeday AND main.ticker = aux.ticker AND main.modified = aux.modified
+ ORDER BY tradeday, ticker);
